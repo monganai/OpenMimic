34d9a74de63bd6ce4e971f7a05fa3f8403ea2532
Aidan mongan <aidan.mongan@datadoghq.com>
2022-10-11 15:09:47 +0100

    Adding OpenMimic

---
+++ openmimic.py
@@ -0,0 +1,11 @@
+from flask import Flask, Response, send_file
+
+app = Flask(__name__)
+
+@app.route("/metrics")
+def passResponse():
+    return send_file("desired_output.txt")
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8888)
